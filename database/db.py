7ec2f06eaac4466dce0371f2f70be7877550336d
Itay Mesh <itaymeshulam2006@gmail.com>
2025-03-24 19:17:18 +0200

    Added a conection to local MongoDB database

--- database/db.py
+++ database/db.py
@@ -1,3 +1,11 @@
 """
-    The connection logic to MySQL.
+    The connection logic to the local MongoDB server.
+    Other files will import this for the database's data
 """
+
+from pymongo import MongoClient
+
+
+#   Create a single instance of the database
+client = MongoClient("mongodb://localhost:27017/")
+db = client["RecipeWebsite"]
