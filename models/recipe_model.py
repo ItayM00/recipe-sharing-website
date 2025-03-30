a436c39c4b5385a9be1e9b2c57d7a0702ebbf42f
Itay Mesh <itaymeshulam2006@gmail.com>
2025-03-30 19:00:42 +0300

    Added functions to recipe model for handling database

--- models/recipe_model.py
+++ models/recipe_model.py
@@ -1,5 +1,63 @@
 """
     Database logic
-    recipe.py → Handles recipe-related database functions.
+    recipe_model.py → Handles recipe-related database functions.
 """
-from database.db import db
+from database.db import db 
+
+recipe_collection = db['recipes']
+
+
+def create_recipe(recipe) -> bool:
+    result = recipe_collection.insert_one(recipe)
+
+    return bool(result.inserted_id)
+
+
+def update_recipe(title, user_email, new_recipe) -> bool:
+    result = recipe_collection.update_one(
+        {'title': title, 'creator_email':user_email},
+        {'$set': new_recipe}
+    )
+    
+    return result.modified_count > 0
+
+
+
+def delete_recipe(title, user_email) -> bool:
+    result = recipe_collection.delete_one({'title':title, 'creator_email':user_email})
+
+    return result.deleted_count > 0
+
+
+
+def get_all_recipes() -> list:
+    return list(recipe_collection.find())
+
+
+
+def get_recipe_by_user(title, user_email) -> dict:
+    recipe = recipe_collection.find_one({'creator_email':user_email, 'title':title})
+
+    if recipe:
+        return recipe
+    else:
+        return {'msg':'recipe not found!'}
+    
+
+def get_all_recipes_by_user(user_email) -> list:
+    return list(recipe_collection.find({'creator_email':user_email}))
+
+
+
+def get_recipe_by_title(recipe_title) -> dict:
+    recipe = recipe_collection.find_one({'title':recipe_title})
+
+    if recipe:
+        return recipe
+    else:
+        return {'msg':'recipe not found!'}
+
+
+
+def get_recipes_by_category(category) -> list:
+    return list(recipe_collection.find({'category':category}))
--- models/user_model.py
+++ models/user_model.py
@@ -1,6 +1,6 @@
 """
     Database logic
-    user.py → Handles user-related database functions.
+    user_model.py → Handles user-related database functions.
 """
 from database.db import db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 users_collection = db["users"]
 
 
-def register_user(user):
+def register_user(user) -> bool:
     """
     All the tests for correct input will be done in the js file
     """
@@ -21,11 +21,15 @@ def register_user(user):
     user["password"] = hashed_password
 
     # Insert user to the database
-    users_collection.insert_one(user)
-    return True
+    result = users_collection.insert_one(user)
+
+    if result.inserted_id:
+        return True
+    else:
+        return False
     
 
-def login_user(email, password):
+def login_user(email, password) -> bool:
     """
     Authenticate user through email from the database, return appropriate msg
     """
@@ -37,14 +41,14 @@ def login_user(email, password):
     return False
 
 
-def get_all_users():
+def get_all_users() -> list:
     """
     Get all the users in the database
     """
-    return users_collection
+    return list(users_collection.find())
     
 
-def get_user_details(email):
+def get_user_details(email) -> dict:
     """
     Get all the user details via email
     """
@@ -53,27 +57,33 @@ def get_user_details(email):
     if user:
         return user
     
-    return {"msg":"Error! email or password incorect"}
+    return {"error":"user not found! email or password incorect"}
 
 
-def update_user_details(email):
+def update_user_details(email, new_user_details):
     user = get_user_details(email)
 
-    if user.get("msg"):
-        return user["msg"]
+    if user.get("error"):
+        return user["error"]
+    
     else:
-        pass # for now
+        result = users_collection.update_one(
+            {'email':email},
+            {'$set':new_user_details}
+        )
+
+        if result.modified_count > 0:
+            return {"success":"successfuly updated user information"}
+        else:
+            {"error":"Error in updating users information"}
 
 
-def delete_user(email):
+def delete_user(email) -> bool:
     """
     delete a user from the database using his email
     """
     result = users_collection.delete_one({"email": email})
 
-    if result.deleted_count > 0:
-        return True # code for "user deleted successfuly"
-    
-    return False # code for "an error of some kind"
+    return result.deleted_count > 0
 
 
