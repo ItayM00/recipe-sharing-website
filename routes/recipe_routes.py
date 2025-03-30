b66e94965a6a1d6a0973cc7ceb85ff5d12b9d336
Itay Mesh <itaymeshulam2006@gmail.com>
2025-03-30 19:15:02 +0300

    Linked recipe creation function to the create page

--- routes/recipe_routes.py
+++ routes/recipe_routes.py
@@ -3,7 +3,9 @@
     recipe_route.py → Handles recipes-related routes.
 """
 
-from flask import Blueprint, redirect, url_for, request, render_template, jsonify
+from flask import Blueprint, redirect, url_for, request, render_template, jsonify, session
+from datetime import date
+from models.recipe_model import create_recipe
 
 
 recipe_bp = Blueprint("recipe", __name__)
@@ -11,14 +13,25 @@ recipe_bp = Blueprint("recipe", __name__)
 
 @recipe_bp.route('/createRecipe', methods=['POST', 'GET'])
 def create_recipe_route():
-    if request.method == 'POST': # submiting a form
+    if request.method == 'POST': # submiting a form (recipe)
         try:
             data = request.json
+
+            if not data:
+                return jsonify({"error": "No data received"}), 400
+
+            data['create_date'] = date.today()
+            data['creator_email'] = session.get('email')
+
+            if not data['creator_email']:
+                return jsonify({"error": "User not logged in"}), 401
+
             print("Received Recipe Data:", data)
 
-            # Process the data (saving it to the database here)
-            
-            return redirect(url_for('home.home_route'))
+            if create_recipe(data):
+                return redirect(url_for('home.home_route'))
+            else:
+                return jsonify({"error": "Error in creating the recipe"}), 401
         
         except Exception as e:
             return jsonify({"error": str(e)}), 400
